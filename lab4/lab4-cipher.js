b1f49d3011c93dd6b7ceb847b91a9605762c2d8a
Yury Kozhenov <yurykozhenov@gmail.com>
2017-10-14 15:31:42 +0300

    Fix and refactor lab4

--- lab4/lab4-cipher.js
+++ lab4/lab4-cipher.js
@@ -1,14 +1,13 @@
 window.Cipher4 = function() {
   class Cipher4 {
     static encryptTransposition(input, key, placeholder) {
-      return _(input)
-        .chunk(key.length)
-        .map(row => row.length < key.length
-          ? row.concat(_.fill(new Array(key.length - row.length), placeholder))
-          : row)
-        .map(row => row.map((char, index) => row[key[index]]))
-        .flatten()
-        .join('');
+      return _.flow(
+        _.bind(_.chunk, _, _, key.length),
+        _.bind(this._transform, this, _, key, placeholder),
+        this._transpose,
+        _.flatten,
+        _.bind(_.join, _, _, '')
+      )(input);
     }
 
     static decryptTransposition(input, key, placeholder) {
@@ -18,7 +17,25 @@ window.Cipher4 = function() {
         reversedKey[num] = index;
       });
 
-      return this.encryptTransposition(input, reversedKey, placeholder);
+      return _.flow(
+        _.bind(_.chunk, _, _, Math.ceil(input.length / key.length)),
+        this._transpose,
+        _.bind(this._transform, this, _, reversedKey, placeholder),
+        _.flatten,
+        _.bind(_.join, _, _, '')
+      )(input);
+    }
+
+    static _transform(input, key, placeholder) {
+      return input
+        .map(row => row.length < key.length
+          ? row.concat(_.fill(new Array(key.length - row.length), placeholder))
+          : row)
+        .map(row => row.map((char, index) => row[key[index]]));
+    }
+
+    static _transpose(arr) {
+      return _.zip(...arr);
     }
   }
 
